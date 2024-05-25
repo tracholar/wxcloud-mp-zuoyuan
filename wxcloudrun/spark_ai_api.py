d85245baa8b3b3feeb37839fa510de2fb51d51c6
zuoyuan.zy <zuoyuan.zy@bytedance.com>
2024-05-25 23:28:35 +0800

    add spark ai support, limit 150 word

--- wxcloudrun/spark_ai_api.py
+++ wxcloudrun/spark_ai_api.py
@@ -38,7 +38,7 @@ def chat(msg):
     )
     messages = [ChatMessage(
         role="user",
-        content=msg
+        content="请在150字以内回复我。" + msg
     )]
     handler = MyHandler()
     a = spark.generate([messages], callbacks=[handler])
